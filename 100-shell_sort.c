ba585e4862489b8c9a3db5e2f3a6c66c25abb9c0
Oliver Njiru <olivernjiru@gmail.com>
2023-01-25 13:57:19 +0300

    update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,46 +1,46 @@
 #include "sort.h"
+
 /**
- * shell_sort - function that sorts an array
- * of integers in ascending order using the
- * Shell sort algorithm
- *
- * @array: input arrray
- * @size: size of the array
- * Return: no return
+ * shell_sort - shell sorting algorithm using insertion sort on subarrays
+ * @array: array to sort
+ * @size: size of array
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t n = 1;
+	size_t gap, i, j, k;
+	int tmp;
 
-	while (n < size)
-		n = (n * 3) + 1;
-
-	while ((n = (n - 1) / 3) > 0)
-		_ssort(array, size, n);
-}
-/**
- * _ssort - auxiliar function for
- * shell_sort function
- * @a: input arrray
- * @size: size of the array
- * @n: intervale
- * Return: no return
- */
-void _ssort(int *a, int size, int n)
-{
-	int tmp, i, j;
-
-	for (i = 0; (i + n) < size; i++)
+	for (gap = 1; gap < size; gap = 3 * gap + 1)
+		;
+	for (gap /= 3; gap > 0; gap /= 3)
 	{
-		for (j = i + n; (j - n) >= 0; j = j - n)
+		for (i = 0; i < gap; i++)
 		{
-			if (a[j] < a[j - n])
+			j = i;
+			while (j < size - gap)
 			{
-				tmp = a[j];
-				a[j] = a[j - n];
-				a[j - n] = tmp;
+				if (array[j] > array[j + gap])
+				{
+					tmp = array[j];
+					array[j] = array[j + gap];
+					array[j + gap] = tmp;
+					for (k = j; k >= gap; k -= gap)
+					{
+					if (array[k] < array[k - gap])
+					{
+					tmp = array[k];
+					array[k] = array[k - gap];
+					array[k - gap] = tmp;
+					}
+					else
+					break;
+					}
+				}
+				else
+					j += gap;
 			}
 		}
+		print_array(array, size);
+
 	}
-	print_array(a, size);
 }
